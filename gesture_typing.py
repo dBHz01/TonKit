5ab41a5214464dc23c9c7431d3d5998c5c07653c
hez19 <dbhz2001@gmail.com>
2022-03-25 13:49:14 +0800

    ✨ feat(plot&gesture_typing): add webplot and way of choosing word

---
+++ gesture_typing.py
@@ -0,0 +1,223 @@
+import numpy as np
+from math import sqrt
+import matplotlib.pyplot as plt
+from queue import PriorityQueue
+from dtaidistance import dtw_ndim
+
+LETTER_IN_KEYBOARD = [['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+                      ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
+                      ['Z', 'X', 'C', 'V', 'B', 'N', 'M']]
+WORD_SET = set()
+DIST_PATTERN = {}
+STD_KB_POS = {}
+WORD_PROB = {}
+
+
+def euclidean_distance(p: np.ndarray, q: np.ndarray) -> float:
+    return sqrt((p[0] - q[0])**2 + (p[1] - q[1])**2)
+
+
+def init_all(reshape_paras):
+    init_word_set(1000)
+    init_keyboard_pos(reshape_paras)
+    init_pattern_set()
+
+
+def init_word_set(num):
+    global WORD_PROB
+    with open("./data/words_10000.txt", 'r') as file:
+        lines = file.readlines()
+        for i in range(len(lines)):
+            if (i >= num):
+                return
+            word = lines[i].lower().replace('\n', '').split('\t')[0]
+            prob = float(lines[i].lower().replace('\n', '').split('\t')[2])
+            WORD_SET.add(word)
+            WORD_PROB[word] = prob
+
+
+def generate_standard_pattern(word: str, num_nodes: int, distribute):
+    word = word.upper()
+    nodes = []
+    pattern = []
+    for i, c in enumerate(word):
+        if (i > 0 and word[i] == word[i - 1]):
+            continue
+        nodes.append(STD_KB_POS[c])
+    if len(nodes) == 1:
+        return [nodes[0] for i in range(num_nodes)]
+    total_len = 0
+    for i in range(len(nodes) - 1):
+        total_len += euclidean_distance(nodes[i], nodes[i + 1])
+    num_pieces = num_nodes - 1
+    used_pieces = 0
+    for i in range(len(nodes) - 1):
+        if i == len(nodes) - 2:
+            p1 = num_pieces - used_pieces
+        else:
+            d1 = euclidean_distance(nodes[i], nodes[i + 1])
+            p1 = int(d1 * num_pieces / total_len)
+        if p1 == 0:
+            continue
+        delta_x = nodes[i + 1][0] - nodes[i][0]
+        delta_y = nodes[i + 1][1] - nodes[i][1]
+        for j in range(0, p1):
+            pattern.append(
+                np.array([
+                    nodes[i][0] + delta_x * distribute(j / p1),
+                    nodes[i][1] + delta_y * distribute(j / p1)
+                ]))
+        used_pieces += p1
+    pattern.append(nodes[-1])
+    if len(pattern) != num_nodes:
+        print(word, num_nodes, len(pattern))
+        raise Exception()
+    return np.array(pattern)
+
+
+def init_pattern_set():
+    global DIST_PATTERN
+    for w in WORD_SET:
+        dist_path = generate_standard_pattern(
+            w, int((len(w) * 6.6457 + 4.2080) / 2),
+            lambda x: -2 * x**3 + 3 * x**2)
+        dist_path_x = [d[0] for d in dist_path]
+        dist_path_y = [d[1] for d in dist_path]
+        DIST_PATTERN[w] = list(zip(dist_path_x, dist_path_y))
+
+
+def init_keyboard_pos(reshape_paras):
+    '''
+    This function generates linear keyboard position
+    '''
+    global STD_KB_POS
+    # q_pos = np.array([0.5 * (0.9 - 0.1) / 10 + 0.1, 0.85])
+    # p_pos = np.array([-0.5 * (0.9 - 0.1) / 10 + 0.9, 0.85])
+    # a_pos = np.array([0.5 * (0.8 - 0.2) / 9 + 0.2, 0.5])
+    # l_pos = np.array([-0.5 * (0.8 - 0.2) / 9 + 0.8, 0.5])
+    # z_pos = np.array([0.5 * (0.75 - 0.25) / 9 + 0.25, 0.15])
+    # m_pos = np.array([-0.5 * (0.75 - 0.25) / 9 + 0.75, 0.15])
+    q_pos = np.array([reshape_paras[0], 0.85])
+    p_pos = np.array([reshape_paras[1], 0.85])
+    a_pos = np.array([reshape_paras[2], 0.5])
+    l_pos = np.array([reshape_paras[3], 0.5])
+    z_pos = np.array([reshape_paras[4], 0.15])
+    m_pos = np.array([reshape_paras[5], 0.15])
+    pos = {}
+    pos.update({letter: ((9 - i) * q_pos + i * p_pos) / 9
+               for i, letter in enumerate(LETTER_IN_KEYBOARD[0])})
+    pos.update({letter: ((8 - i) * a_pos + i * l_pos) / 8
+               for i, letter in enumerate(LETTER_IN_KEYBOARD[1])})
+    pos.update({letter: ((6 - i) * z_pos + i * m_pos) / 6
+               for i, letter in enumerate(LETTER_IN_KEYBOARD[2])})
+    STD_KB_POS = pos
+
+
+def resample_path(path):
+    sampleSize = 50
+    n = len(path)
+    ret = []
+    if (n == 1):
+        for i in range(sampleSize):
+            ret.append(path[0])
+        return ret
+    length = 0
+    for i in range(n-1):
+        length += euclidean_distance(np.array(path[i]), np.array(path[i + 1]))
+    interval = length / (sampleSize - 1)
+    lastPos = path[0]
+    currLen = 0
+    no = 1
+    ret.append(path[0])
+    while (no < n):
+        dist = euclidean_distance(np.array(lastPos), np.array(path[no]))
+        if (currLen + dist >= interval and dist > 0):
+            ratio = (interval - currLen) / dist
+            tmpPos = lastPos.copy()
+            lastPos = [
+                tmpPos[0] + ratio * (path[no][0] - tmpPos[0]),
+                tmpPos[1] + ratio * (path[no][1] - tmpPos[1]),
+            ]
+            ret.append(lastPos)
+            currLen = 0
+        else:
+            currLen += dist
+            lastPos = path[no]
+            no += 1
+    for i in range(len(ret), sampleSize):
+        ret.append(path[n - 1])
+    return ret
+
+
+def check_top_k(data, k):
+    '''
+    data = [[x, y, z], ...]
+    '''
+    data = np.array(data)
+    print(data)
+    total = 0
+    # top_k = [0] * k
+    prev = None
+    if data is None:
+        return
+    user_path_x = data[:, 0]
+    user_path_y = data[:, 1]
+    depths = data[:, 2]
+    # user = genVectors(x, y, depths)
+    # word = get_word(i, j)
+    total += 1
+    # x, y = downsample(x, y, depths, 2)
+    user_path = np.array(list(zip(user_path_x, user_path_y)))
+    q = PriorityQueue()
+
+    for w in WORD_SET:
+        # theta distance
+        # pattern = THETA_PATTERN[w]
+        # if (len(user) <= 0 or len(pattern) <= 0):
+        #     continue
+        # d1, cost_matrix, acc_cost_matrix, path = a_dtw(
+        #     user, pattern, dist=distance)
+
+        # shape distance
+        pattern = DIST_PATTERN[w]
+        if (len(user_path) <= 0 or len(pattern) <= 0):
+            continue
+        d2 = dtw_ndim.distance_fast(user_path, np.array(
+            pattern), use_pruning=True, window=8)
+        
+        if (w in WORD_PROB):
+            d1 = np.log10(WORD_PROB[w])
+        
+        # # bigram probabilities
+        # if prev in BIWORD_PROB and w in BIWORD_PROB[prev]:
+        #     d = BIWORD_PROB[prev][w]
+        # elif w in WORD_PROB:
+        #     d = WORD_PROB[w]
+        # else:
+        #     d = 10**(-10)
+
+        # q.put((0.6 * d2 - 0.4 * 0.1 * np.log10(d), w))
+        q.put((0.4 * 0.1 * d1 + 0.6 * d2, w))
+
+    top = []
+    for p in range(k):
+        try:
+            tmp = q.get_nowait()
+            top.append(tmp[1])
+            # if word in top:
+            #     top_k[p] += 1
+        except:
+            break
+    # print(word, top[:10])
+    print(top[:10])
+    # prev = word
+    print("total: %d" % total)
+    # for i in range(k):
+    #     print("top%d acc: %f" % (i + 1, top_k[i] / total))
+    return top[:k]
+
+
+if __name__ == "__main__":
+    init_word_set()
+    init_keyboard_pos()
+    check_top_k()
--- plot.py
+++ plot.py
@@ -1,20 +1,93 @@
 '''
     plot the pressure point on screen
 '''
-
+from socket import socket, AF_INET, SOCK_STREAM, timeout
 import argparse
 import matplotlib.pyplot as plt
 import numpy as np
 import json
 import time
+from multiprocessing import Process
+try:
+    import thread
+except ImportError:
+    import _thread as thread
 from matsense.tools import load_config, make_action
 from matsense.uclient import Uclient
 from matsense.process import Processor
+from gesture_typing import check_top_k, init_all
 
 BASE_DATA_DIR = "./data/"
 LETTER_IN_KEYBOARD = [['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', ''],
                       ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ''],
                       ['Z', 'X', 'C', 'V', 'B', 'N', 'M', '']]
+
+# shared variable in threads
+is_recording = False
+gesture_typing_data = []
+stop_showing = False
+current_data = None
+
+
+IP = "localhost"
+PORT = 8081
+
+
+class CursorClient:
+    def __init__(self, server_addr, port, timeout=1):
+        self.my_socket = socket(AF_INET, SOCK_STREAM)
+        self.my_socket.settimeout(timeout)
+        self.connect(server_addr, port)
+
+    def __exit__(self, type, value, traceback):
+        self.close()
+
+    def connect(self, address, port):
+        self.my_socket.connect((address, port))
+        print(f"client connecting to server: {address}")
+
+    def close(self):
+        self.my_socket.close()
+        print("remote client socket closed")
+
+    def sendToKB(self, touch_state, x, y):
+        paras = [touch_state, x, y]
+        self.my_socket.send(
+            str(" ".join([str(item) for item in paras]) + "\n").encode())
+
+    def sendToKBPlot(self, type, touch_state, x, y):
+        # type should be in ['event', 'select', 'reshape']
+        paras = [type, touch_state, x, y]
+        self.my_socket.send(
+            str(" ".join([str(item) for item in paras]) + "\n").encode())
+
+    def selectWord(self, selectDirection):
+        paras = ["select", selectDirection]
+        self.my_socket.send(
+            str(" ".join([str(item) for item in paras]) + "\n").encode())
+
+    def reshapeKB(self, pos):
+        # pos should be [0-1] * 12, as q_pos.x, q_pos.y, p_pos.x ...
+        paras = ['reshape'] + pos
+        self.my_socket.send(
+            str(" ".join([str(item) for item in paras]) + "\n").encode())
+
+    def setCandidates(self, cands):
+        # len(cands) should >= 5
+        paras = ['candidates'] + cands
+        while (len(paras) < 6):
+            paras.append(paras[-1])
+        self.my_socket.send(
+            str(" ".join([str(item) for item in paras]) + "\n").encode())
+
+    def sendButton(self, cmd):
+        self.my_socket.send((cmd+"\n").encode())
+
+    def sendPressure(self, pressure):
+        self.my_socket.send((str(pressure)+"\n").encode())
+
+
+my_remote_handle = CursorClient(IP, PORT)
 
 
 def mock_generator():
@@ -71,39 +144,64 @@ def draw_border():
                      [i * (0.9 - 0.1) / 10 + 0.1, 1]] for i in range(11)])
     col_2 = np.array([[[i * (0.8 - 0.2) / 9 + 0.2, 0.3],
                      [i * (0.8 - 0.2) / 9 + 0.2, 0.7]] for i in range(10)])
-    col_3 = np.array([[[i * (0.65 - 0.35) / 7 + 0.35, 0],
-                     [i * (0.65 - 0.35) / 7 + 0.35, 0.3]] for i in range(8)])
+    col_3 = np.array([[[i * (0.75 - 0.25) / 7 + 0.25, 0],
+                     [i * (0.75 - 0.25) / 7 + 0.25, 0.3]] for i in range(8)])
     col = [col_1, col_2, col_3]
     for line in row:
         plt.plot(line[:, 0], line[:, 1], color='#000000')
-    gap_lengths = [(0.9 - 0.1) / 10, (0.8 - 0.2) / 9, (0.65 - 0.35) / 7]
+    gap_lengths = [(0.9 - 0.1) / 10, (0.8 - 0.2) / 9, (0.75 - 0.25) / 7]
     for i, c in enumerate(col):
         gap_length = gap_lengths[i]
         for j, line in enumerate(c):
             plt.plot(line[:, 0], line[:, 1], color='#000000')
             # 0.01 for half size of the letter
             plt.text(line[0][0] + gap_length / 2 - 0.01,
-                     np.average([line[0][1], line[1][1]]), LETTER_IN_KEYBOARD[i][j])
+                     np.average([line[0][1], line[1][1]]
+                                ), LETTER_IN_KEYBOARD[i][j],
+                     fontsize=15)
     plt.show()
 
 
-def scatter_plot():
+def scatter_plot(my_generator, output_filename):
+    global is_recording, gesture_typing_data
 
     plt.ion()
 
+    draw_border()
+
+    prev_point = None
+
     for _ in my_generator:
+        if stop_showing:
+            break
+
+        # generate data
+        row, col, val = next(my_generator)
+
         # clean previous picture
         plt.cla()
         draw_border()
-
-        row, col, val = next(my_generator)
 
         # record all data
         if (output_filename != None):
             with open(BASE_DATA_DIR + output_filename, "a") as file:
                 file.write(json.dumps([row, col, val]) + "\n")
 
+        plt.xlim(0, 1)
+        plt.ylim(0, 1)
+
+        # my_x_ticks = np.arange(0, 1, 0.1)
+        # my_y_ticks = np.arange(0, 1, 0.1)
+        # plt.xticks(my_x_ticks)
+        # plt.yticks(my_y_ticks)
+
         if (val > 0.5):
+            # if begin recording, then record data to gesture_typing_data
+            # else clean it
+            if (is_recording):
+                gesture_typing_data.append([row, col, val])
+            else:
+                gesture_typing_data = []
             # x, y = point_to_movement(row, col, val)
             # l = np.linalg.norm([x, y])
             x_index = np.array(row)
@@ -111,10 +209,11 @@ def scatter_plot():
 
             color_list = np.array(val)
 
-            plt.xlim(0, 1)
-            plt.ylim(0, 1)
+            plt.scatter(x_index, y_index, c=color_list, marker="o", s=20)
 
-            plt.scatter(x_index, y_index, c=color_list, marker="o")
+            # if (prev_point != None):
+            #     plt.plot([x_index, prev_point[0]], [y_index, prev_point[1]], '--', color="#000000", linewidth=2.5)
+            prev_point = [x_index, y_index]
 
         plt.pause(0.05)
 
@@ -124,9 +223,105 @@ def scatter_plot():
     return
 
 
-def main():
+def get_reshape_paras():
+    global current_data
+    reshape_paras = []
+    pos_name = ['q_pos', 'p_pos', 'a_pos', 'l_pos', 'z_pos', 'm_pos']
+    pos = [np.array([0.5 * (0.9 - 0.1) / 10 + 0.1, 0.85]), np.array([-0.5 * (0.9 - 0.1) / 10 + 0.9, 0.85]), np.array([0.5 * (0.8 - 0.2) / 9 + 0.2, 0.5]),
+           np.array([-0.5 * (0.8 - 0.2) / 9 + 0.8, 0.5]), np.array([0.5 * (0.75 - 0.25) / 9 + 0.25, 0.15]), np.array([-0.5 * (0.75 - 0.25) / 9 + 0.75, 0.15])]
+    i = 0
+    while (len(reshape_paras) < 6):
+        input("please press " + pos_name[i])
+        # (q_pos, p_pos, a_pos, l_pos, z_pos, m_pos) .x
+        row = current_data[0]
+        reshape_paras.append(max(row, pos[i][0]) if (i % 2 == 0)  else min(row, pos[i][0]))
+        print(row)
+        i += 1
+    
+    # for reshaping the keyboard
+    my_remote_handle.reshapeKB([reshape_paras[0], 0.85,
+                                reshape_paras[1], 0.85,
+                                reshape_paras[2], 0.5,
+                                reshape_paras[3], 0.5,
+                                reshape_paras[4], 0.15,
+                                reshape_paras[5], 0.15])
+    return reshape_paras
+
+
+def web_plot(my_generator, output_filename):
+    global my_remote_handle, current_data
+
+    global is_recording, gesture_typing_data
+    for _ in my_generator:
+        if stop_showing:
+            break
+
+        # generate data
+        row, col, val = next(my_generator)
+        current_data = [row, col, val]
+
+        # record all data
+        if (output_filename != None):
+            with open(BASE_DATA_DIR + output_filename, "a") as file:
+                file.write(json.dumps([row, col, val]) + "\n")
+
+        if (val > 0.5):
+            my_remote_handle.sendToKBPlot("event", 2, row, col)
+            # if begin recording, then record data to gesture_typing_data
+            # else clean it
+            if (is_recording):
+                gesture_typing_data.append([row, col, val])
+            else:
+                gesture_typing_data = []
+        else:
+            my_remote_handle.sendToKBPlot("event", 2, -1, -1)
+            
+        time.sleep(0.01)
+    return
+
+
+def interactive_mode():
+    global is_recording, gesture_typing_data, stop_showing, my_remote_handle, current_data
+    while True:
+        inst = input()
+        if (inst == "r"):
+            # begin recording
+            is_recording = True
+        elif (inst == "s"):
+            # stop recording and check top_k
+            is_recording = False
+            top_k = check_top_k(gesture_typing_data, 10)
+            my_remote_handle.setCandidates([i for i in top_k[:5]])
+            gesture_typing_data = []
+        elif (inst == "e"):
+            stop_showing = True
+        elif (inst == 'c'):
+            # choose word
+            if (current_data[0] < 0.5):
+                if (current_data[1] < 0.5):
+                    my_remote_handle.selectWord('left')
+                else:
+                    my_remote_handle.selectWord('up')
+            else:
+                if (current_data[1] < 0.5):
+                    my_remote_handle.selectWord('down')
+                else:
+                    my_remote_handle.selectWord('right')
+                    
+
+        time.sleep(0.1)
+
+
+def main(my_generator):
     # cali_each_point()
-    scatter_plot()
+    # scatter_plot()
+    # p = Process(target=web_plot, args=(my_generator, output_filename))
+    # p.start()
+    thread.start_new_thread(web_plot, (my_generator, output_filename))
+    reshape_paras = get_reshape_paras()
+    print("reshape_paras", reshape_paras)
+    init_all(reshape_paras)
+    interactive_mode()
     # draw_border()
 
 
@@ -167,4 +362,4 @@ if __name__ == "__main__":
         else:
             my_generator = my_processor.gen_points(
                 my_processor.gen_wrapper(tongue_client.gen()))
-        main()
+        main(my_generator)
